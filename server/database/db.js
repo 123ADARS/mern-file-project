1664a2762de0d2bffabb17e45c096381ff98bdf1
adarsh <adarshsingh6861@gmail.com>
2023-09-23 00:42:52 +0530

    yes

--- server/database/db.js
+++ server/database/db.js
@@ -1,14 +1,7 @@
-import mongoose from "mongoose";
-
-
-
+import mongoose from "mongoose"
 const DBConnection = async () => {
-    // const USERNAME = 'adarshsingh';
-    // const PASSWORD = 'Adarsh9060'
-
-    //const MONGO_URI = "mongodb://adarshsingh:Adarsh9060@@ac-j82cyvr-shard-00-00.ck1ryn0.mongodb.net:27017,ac-j82cyvr-shard-00-01.ck1ryn0.mongodb.net:27017,ac-j82cyvr-shard-00-02.ck1ryn0.mongodb.net:27017/?ssl=true&replicaSet=atlas-k3ax8h-shard-0&authSource=admin&retryWrites=true&w=majority";
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/test", 
+        await mongoose.connect("mongodb+srv://user2000:123test@project-ecommerce.v1eswgy.mongodb.net/project-ecommerce",
         { useNewUrlParser: true , 
           useUnifiedTopology: true
     });
